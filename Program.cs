0887ed4b8f978cb49563fe67eb0bcc21df88751f
George Ter-Saakov <gevorgter@gmail.com>
2022-03-11 17:48:14 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using var f = new StreamReader("test.txt");
+            var s = f.ReadToEnd();
+            Console.WriteLine(s.IndexOf("("));
+            Console.ReadLine();
+        }
+    }
+}
